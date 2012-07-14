0d4e3fbc759b03a39ff2d7a2073c08b803a72b02
sidgrover <sidharthagrover@gmail.com>
2012-07-13 22:31:52 -0400

    Drop tables script

---
+++ Data/DropTables.sql
@@ -0,0 +1,13 @@
+-- This script removes all the tables from the database
+
+Drop table GeneralConfiguration;
+Drop table StrategyBuyList;
+Drop table StrategyLinkedPosition;
+Drop table StratTrader.Order;
+Drop table Strategy;
+Drop table DailyHistoryBar;
+Drop table InstrumentConfiguration;
+Drop table Instrument;
+Drop table Exchange;
+Drop table Country;
+Drop table Sector;
